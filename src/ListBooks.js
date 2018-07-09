1d9ad80428c53a919fc4e8d95f34051b2d642075
Beto <adbertomelo@gmail.com>
2018-07-09 17:11:50 -0300

    correção do erro ao buscar capa do livro

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -35,7 +35,10 @@ class ListBooks extends Component{
                           <li key={book.id}>
                           <div className="book">
                             <div className="book-top">
-                              <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                              <div className="book-cover" style={{ width: 128, 
+                                                                  height: 193, 
+                                                                  backgroundImage: book.imageLinks ? `url(${book.imageLinks.thumbnail})` : "" }}>
+                                                                  </div>
                               <div className="book-shelf-changer">
                                 <select defaultValue={book.shelf} onChange={
                                   (event) => {
