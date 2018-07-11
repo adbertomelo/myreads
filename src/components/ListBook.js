04bc5f33b179a1351844105504e6704724ca3870
Beto <adbertomelo@gmail.com>
2018-07-10 22:36:44 -0300

    Correção

--- src/components/ListBook.js
+++ src/components/ListBook.js
@@ -1,4 +1,4 @@
-import React, { Component } from '../../../../../Users/Beto/AppData/Local/Microsoft/TypeScript/2.9/node_modules/@types/react'
+import React, { Component } from 'react'
 
 
 class ListBook extends Component {
